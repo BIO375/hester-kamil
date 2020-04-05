88bedf8bdc13c6cca398bcba588f2bc103288d97
k-hester <kamilhester_2020@depauw.edu>
2020-04-05 03:41:49 -0400

    #Incomplete lab 6

--- Assignments/2020-04-02_Lab6.R
+++ Assignments/2020-04-02_Lab6.R
@@ -27,7 +27,8 @@ tidyverse_update()
 # Read in Craig and Foote 2001) data file
 SalmonColor <- read_csv("datasets/abd/chapter13/chap13q20SalmonColor.csv")
 
-# 13-20a. List two methods that would be appropriate to test whether there was a difference in mean
+# 13-20a. #####
+# List two methods that would be appropriate to test whether there was a difference in mean
 # skin color between the two groups.
 
 # Two methods that would be appropriate are a Mann-Whitney-U test, and a two-sample t-test, because two means are being 
@@ -95,7 +96,8 @@ wilcox.test(skinColor ~ species, data = SalmonColor, alternative = "two.sided", 
 # HA:Kokanee Sockeye salmon mean skin color is different from standard Sockeye salmon. 
 # (Wilcoxon rank sums test: W= 303; P= 6.153e-07)
 
-# 13-20b. Use a transformation to test whether there is a difference in mean between these two
+# 13-20b. #### 
+# Use a transformation to test whether there is a difference in mean between these two
 # groups. Is there a difference in the mean of kokanee and sockeye skin color?
   
 # Add new column of logtransformed skin measurements to the SalmonColor dataset
@@ -132,24 +134,39 @@ ggplot(SalmonColor) +
 ggplot(SalmonColor)+
   geom_qq(aes(sample = skinColor, color = species))
 
-# Anaylsis: A right skew indicated in the standaed sockeye histograms. 
-#The boxplots also indicate some right skew in the sockeye group: there is a high outlier, however it appears the lower
-# whisker is longer than the upper. 
-# The q-q plot shows that for both groups the data mostly falls on a line. 
-# However the sample sizes are is large so a parametric test is still OK.
+# Anaylsis: The log transformation appears to have normalized the sockeye boxplot 
 
 # Two-sided
 t.test(skinColor ~ species, data = SalmonColor, var.equal = TRUE, alternative = "two.sided", conf.level = 0.95)
 
 # HA:Kokanee Sockeye salmon mean skin color is different from standard Sockeye salmon. 
-# (Two-sided Two-sample t-test: t= 10.297; df= 33; P= 7.747e-12)
+# (Two-sided Two-sample t-test: t= 12.133; df= 33; P= 1.038e-13)
 
+### 13-25. ####
+# Test whether there is a change in biomass of rainforest areas following clear-cutting.
 
+# readLaurance et al. (1997) file 
+Clearcuts <- read_csv("datasets/abd/chapter13/chap13q25Clearcuts.csv")
 
+# The most appropriate test for whether there is change in biomass within rainforest areas is
+# the non-parametric sign test, because the null hypothesis of this question that the median of difference between
+# the different areas will be 0. A sign test also shows perference one way or another. 
 
-# 13c.
-# 13d.
-# 13e.
-# 13f. 
+# One-sample, Two-sided
+SignTest(Clearcuts$biomassChange, 
+         alternative = "two.sided", mu = 0, conf.level = 0.95)
+# H0: The median of differences of change in biomass is zero. (S= 21; number of differences=36; P= 0.405)
+
+### 13-26. ####
+# Choose an appropriate method and test whether females preferred one type of male over the
+# other type.
+
+### Review 2-16a. ####
+# With these data, estimate the magnitude of the effect of the mutation (the difference between the means) 
+# on the amount of time spent in aggressive activity. Put appropriate bounds on your estimate of the effect.
+
+### Review 2-16b. ####
+# What is the weight of evidence that this effect is not zero? Perform an appropriate
+# statistical test of the difference.
 
 

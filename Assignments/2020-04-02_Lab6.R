ac695affbe61b7fcd8fb6e1ce3135fef5832aee5
k-hester <kamilhester_2020@depauw.edu>
2020-04-05 20:33:24 -0400

    # lab 6, completed but unedited

--- Assignments/2020-04-02_Lab6.R
+++ Assignments/2020-04-02_Lab6.R
@@ -161,12 +161,96 @@ SignTest(Clearcuts$biomassChange,
 # Choose an appropriate method and test whether females preferred one type of male over the
 # other type.
 
+# Read in (Blount et al. 2003) file. 
+ZebraFinchBeaks <- read_csv("datasets/abd/chapter13/chap13q26ZebraFinchBeaks.csv")
+
+# HA: Female Zebra Finches have a preference for carotenoid-supplemented male Zebra Finches. 
+# H0: Female Zebra Finches have no preference for carotenoid-supplemented male Zebra Finches. 
+# Because the null hypothesis is that the means are the same and both higher or lower mean could reject the null the test is two sided
+# A one sample t-test would be most appropriate because its designed to actual mean from one observation per mean with 
+# the mean proposed by the null. 
+
+# One-sample t-test can be calculate using t.test.(Output in the console) 
+# The mu argument gives the value stated in the null hypothesis.
+
+# The code below ASSUMES that you have read in the data file
+# Now you have to specify which dataset the values are coming from using the form dataset$variable_name.
+
+# Two-sided
+t.test(ZebraFinchBeaks$preference, 
+       alternative = "two.sided", mu = 0, conf.level = 0.95)
+
+# Female Zebra Finches have a preference for carotenoid-supplemented male Zebra Finches. 
+# (one sample t-test; t= 5.6198, df= 9, P= 0.0003259)
+
 ### Review 2-16a. ####
 # With these data, estimate the magnitude of the effect of the mutation (the difference between the means) 
 # on the amount of time spent in aggressive activity. Put appropriate bounds on your estimate of the effect.
+
+# HA: The Zebrafish with the spiegeldanio (spd) mutation at the Fgfrla gene spent greater time in aggressive activity than wildtype Zebrafish
+# H0: The Zebrafish with the spiegeldanio (spd) mutation at the Fgfrla gene spent less than or equal time in aggressive activity than wildtype Zebrafish
+
+# The most appropriate test is the two-sample t-test though two means are being compared both experimental treatments
+# were not applied to each experimental unit ruling out at paired t-test
+
+# Read in Norton et al. (2011). file
+ZebrafishAggression <- read_csv("datasets/abd/review2/rev2q16ZebrafishAggression.csv")
+
+# Pooled variances
+# Look at the summary statistics
+summ_Zebrafish <- ZebrafishAggression  %>%
+  group_by(genotype) %>% 
+  summarise(mean_Zebrafish = mean(timeInAggression),
+            sd_Zebrafish = sd(timeInAggression),
+            n_Zebrafish = n())
+
+# Calculate the ratio between the standard deviations as a loose test of homoscedasticity (how normally distributed is the data noise)
+ratio <-(max(summ_Zebrafish$sd_Zebrafish))/(min(summ_Zebrafish$sd_Zebrafish))
+# 1.39, less than 3 so data is normally distributed
+
+# Look at histogram
+ggplot(ZebrafishAggression) +
+  geom_histogram(aes(timeInAggression), binwidth = 10)+
+  facet_wrap(~genotype)
+
+# Generate box plot
+ggplot(ZebrafishAggression) +
+  geom_boxplot(aes(x = genotype, y = timeInAggression))+
+  stat_summary(aes(x = genotype, y = timeInAggression), 
+               fun.y=mean, 
+               colour="blue", 
+               fill = "green",
+               geom="point", 
+               shape=21, 
+               size=3)
+
+# Generate q-q plot
+ggplot(ZebrafishAggression)+
+  geom_qq(aes(sample = timeInAggression, color = genotype))
+
+# Anaylsis: Both histograms, appear to have relatively normal distributions
+# Boxplots indicate some leftward skew in the wild type group: the upper whisker is longer than 
+# the lower whisker, and the mean is smaller than the median.  
+# In the Q-Q plot, the extreme points do not lay in a line, there is not a large sample size for either group n1=10 & n2=11.
+# Because, the two-sample t-test is a robust test that can withstand minor violations we are okay to proceed with this parametric test. 
+
+
+# NOTE: Group 1 and Group 2 are ordered alphabetically unless you specify otherwise
+# In the output of the t-test, the first mean under "sample estimates" is group 1, the second is group 2
+# One-sided, HA is greater than 0
+t.test(timeInAggression ~ genotype, data = ZebrafishAggression, var.equal = TRUE, alternative = "greater", conf.level = 0.95)
+# The Zebrafish with the spiegeldanio (spd) mutation at the Fgfrla gene spent greater time in aggressive activity than wildtype Zebrafish.
+# (one-sided two-sample t-test: t= 3.3802; df= 19; P= 0.001571)
 
 ### Review 2-16b. ####
 # What is the weight of evidence that this effect is not zero? Perform an appropriate
 # statistical test of the difference.
 
+# The most appropriate statistical test to determine if the effect shown is not zero is a non-parametric Mann-Whitney U test 
+# This is because the null hypothesis of this question that the distribution between the mutant and 
+# wildtype zebrea will identical. A test can show the shift in distrution if HA is true and there is a non zero effect.
 
+# Two-sided
+wilcox.test(timeInAggression ~ genotype, data = ZebrafishAggression, alternative = "two.sided", conf.level = 0.95)
+# The Zebrafish with the spiegeldanio (spd) mutation at the Fgfrla gene spent statistically different amounts of time in aggressive activity than wildtype Zebrafish.
+# (Two-sided Wilcoxon rank sum test W= 90; P= 0.01272)
